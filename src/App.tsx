a867a34a2996b21737546464116daf717040e51a
takuma123-type <seishirou1270@gmail.com>
2023-03-06 22:00:59 +0900

    scssの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+         Discord
+        </h1>
       </header>
     </div>
   );
