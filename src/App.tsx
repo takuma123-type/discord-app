6879dc60fd86d60758030c7ef725aa4e40cc9631
takuma123-type <seishirou1270@gmail.com>
2023-03-13 02:02:31 +0900

    ログイン機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
+import Login from './components/login/Login';
+import { useAppDispatch, useAppSelector } from './app/hook';
+import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
 
 function App() {
+  const user = useAppSelector((state) => state.user);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if(loginUser) {
+        dispatch(
+          login({
+          uid: loginUser.uid,
+          photo: loginUser.photoURL,
+          email: loginUser.email,
+          displayname: loginUser.displayName,
+        })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="App">
-      {/* sidebar */}
-      <Sidebar />
+      {user ? 
+      (
+        <>
+          <Sidebar />
 
-      {/* chat */}
-      <Chat />
+          <Chat />
+        </>
+      ): (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
